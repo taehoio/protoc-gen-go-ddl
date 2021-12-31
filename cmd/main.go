76e79e8c53b07704591b8042b1ff6b6ad8f96144
Taeho Kim <taeho@taeho.io>
2021-12-31 21:44:52 +0900

    Add protoc-gen-go-ddl (#1)

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/taehoio/protoc-gen-go-ddl/internal/ddl"
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+const version = "0.0.1"
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-go-ddl %v\n", version)
+		return
+	}
+
+	var flags flag.FlagSet
+
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+
+		return ddl.GenerateDDLFiles(gen)
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/taehoio/protoc-gen-go-ddl
 
 go 1.17
+
+require (
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/taehoio/ddl v0.0.0-20211231095257-9b8cea58f4c4 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
